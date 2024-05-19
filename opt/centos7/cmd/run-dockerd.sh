051370c801cfd8387f1ca9767cd000a644e5bdff
fenife <fenife@163.com>
2024-05-19 22:18:14 +0800

    mod: split supervisor, add cmd

---
+++ opt/centos7/cmd/run-dockerd.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# set -x
+
+# PID=`ps -ef | grep "$docker_bin" | grep -v grep | awk '{print $2}'`
+
+# https://docs.docker.com/config/daemon/logs/
+PID=`pidof dockerd`
+
+function get_pid {
+    PID=`pidof dockerd`
+    return $PID
+}
+
+
+function start {
+    if [[ "" ==  "$PID" ]]; then
+        dockerd >> /dev/null 2>&1 &
+        echo "dockerd start ..."
+    else
+        echo "dockerd has already started at: $PID"
+    fi
+}
+
+function stop {
+    if [[ "" !=  "$PID" ]]; then
+        echo "kill $PID, dockerd stop ..."
+        kill -9 $PID
+    else
+        echo "dockerd not started"
+    fi
+}
+
+function restart {
+    stop
+    start
+}
+
+function usage {
+    echo "usage:"
+    echo "  $0 {start|stop|restart}"
+}
+
+case "$1" in 
+"start") 
+    start
+    ;;
+"stop")
+    stop
+    ;;
+"restart")
+    restart
+    ;;
+"")
+    usage
+    exit 
+    ;;
+esac
