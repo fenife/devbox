84537fcaaf34db79450fb550b3ee980fcce5e520
fenife <fenife@163.com>
2024-08-18 14:19:55 +0800

    mod: udpate comments

--- build/setup.sh
+++ build/setup.sh
@@ -6,8 +6,14 @@ cd lib/bin
 wget https://golang.google.cn/dl/go1.22.3.linux-amd64.tar.gz
 
 # lib/src
+zsh-5.8.tar.xz
+git-2.30.0.tar.gz  
+openssl-1.1.1w.tar.gz  
+openvswitch-2.13.0.tar.gz  
+Python-3.12.0.tgz  
 
 # lib/vscode
+see lib/vscode/readme.md
 
 
 
