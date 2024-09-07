47af6bdbee792fde11818e208dbf47196c7d4a86
fenife <fenife@163.com>
2024-09-07 14:31:01 +0800

    mod: query dataframe

--- icode/webui/view/base.py
+++ icode/webui/view/base.py
@@ -1,3 +1,5 @@
+from loguru import logger
+from pandas.core.arrays.categorical import contains
 import streamlit as st
 from engine.db import DBClient, DBResult
 from engine.http import HttpClient, HttpResult
@@ -5,14 +7,58 @@ from config import Config
 
 
 class BaseViewer(object):
+
+    def view_dataframe_query(self, data: DBResult):
+        help_str = " f > 1 or f < 2 and f == 3 or f in (1, 2)" \
+                   " and f.str.contains('a') "
+        c = st.container()
+        c_q, c_l, c_w = c.columns(spec=[4, 2, 3])
+        query = c_q.text_input("query",
+                               key=f"df:input:query:{data.dt}",
+                               placeholder=f"query: {help_str}",
+                               label_visibility="collapsed")
+        like_input = c_l.text_input("like",
+                                    key=f"df:input:query:like:{data.dt}",
+                                    placeholder=f"like: field: val",
+                                    label_visibility="collapsed")
+        if like_input:
+            try:
+                field, val = like_input.split(":")
+                like_query = f"{field.strip()}.str.contains('{val.strip()}')"
+                if query:
+                    query += f" {like_query}"
+                else:
+                    query = like_query
+            except Exception as e:
+                st.warning(f"{str(e)}, input: {like_input}")
+
+        df = data.df
+        query_df = None
+        if query:
+            try:
+                logger.info("df query: %s" % query)
+                st.text(f"df.query({query})")
+                query_df = df.query(query)
+            except Exception as e:
+                c.warning(f"{str(e)}")
+
+        return query_df
+
     def view_dataframe(self, data: DBResult):
-        if data:
-            st.write(data.dt, data.sql)
-            st.dataframe(data.df)
+        if not data:
+            return
+
+        df = data.df
+        query_df = self.view_dataframe_query(data)
+        st.write(data.dt, data.sql)
+        if query_df is None:
+            st.dataframe(df)
+        else:
+            st.dataframe(query_df)
 
     def view_http_result(self, r: HttpResult):
         if r is None:
-            return 
+            return
         with st.container(border=True):
             st.write("request:")
             st.code(r.curl(), wrap_lines=True)
